1a004533fce21af05c64ecb01279c97b14d54a4f
powdercat <powdercatone@gmail.com>
2023-08-15 23:24:37 +0900

    add: spring init

---
+++ src/main/java/powder/springskeleton/SpringSkeletonApplication.java
@@ -0,0 +1,13 @@
+package powder.springskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/powder/springskeleton/SpringSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package powder.springskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
