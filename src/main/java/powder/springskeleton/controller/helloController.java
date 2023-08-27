cd102b8411675247f1e5ffe164a94cb6e8e3dab6
powdercat <powdercatone@gmail.com>
2023-08-27 10:13:52 +0900

    add: mvc 패턴, 템플릿 엔진, api 방식

--- src/main/java/powder/springskeleton/controller/helloController.java
+++ src/main/java/powder/springskeleton/controller/helloController.java
@@ -3,6 +3,8 @@ package powder.springskeleton.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -11,5 +13,37 @@ public class helloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring!!!!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 응답 body에 데이터를 넣겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
