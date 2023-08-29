d8955ed0f67739e985d76c78efebaf18d617ed5f
powdercat <powdercatone@gmail.com>
2023-08-29 22:09:26 +0900

    add: member 서비스(비즈니스 메소드) 작성

---
+++ src/main/java/powder/springskeleton/service/MemberService.java
@@ -0,0 +1,51 @@
+package powder.springskeleton.service;
+
+import powder.springskeleton.domain.Member;
+import powder.springskeleton.repository.MemberRepository;
+import powder.springskeleton.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름은 중복 가입 불가
+
+        /*
+        // null 일 가능성이 있으면, optional 로 감싸서 반환한다.
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            // optional 로 감쌌기 때문에 가능. result.get(); 로 바로 꺼내도 되지만, 권장하지 않는다.
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+         */
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
