db2dc346a590834c75d33f3e2f43d0c11acdbc85
powdercat <powdercatone@gmail.com>
2023-09-02 15:52:10 +0900

    add: 회원 목록 컨트롤러 작성

--- src/main/java/powder/springskeleton/controller/MemberController.java
+++ src/main/java/powder/springskeleton/controller/MemberController.java
@@ -2,10 +2,13 @@ package powder.springskeleton.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import powder.springskeleton.domain.Member;
 import powder.springskeleton.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -35,7 +38,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
@@ -43,5 +46,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // members 리스트를 "members" 에 담아서 화면에 넘긴다.
+        // html 에서 model ${members} 를 사용해서 안의 값을 꺼낸다.
+        return "members/memberList";
     }
 }
